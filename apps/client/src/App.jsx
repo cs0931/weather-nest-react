e2ff8133105628e06f5ad38ac67b705515e62b0d
Kiran Davi <kiran_machampilly_from.tp@mom.gov.sg>
2023-05-06 00:22:46 +0800

    display background image in home page

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import { Container } from '@mui/material'
 
 function App() {
-  return <div></div>
+  return (
+    <div className="bg_img">
+      <Container></Container>
+    </div>
+  )
 }
 
 export default App
